90ce8ebd15d6bd5882d24277d9451598de0d17c2
Taiye Salami <taiye41@gmail.com>
2019-05-16 11:11:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
 function destructivelyAppendKitten(kittens, Ralph) {
-  return kittens = ["Ralph", ...kittens]
+  return kittens.push('Ralph') 
 }
