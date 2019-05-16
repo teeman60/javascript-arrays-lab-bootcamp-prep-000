fea0497987c2d98555be55e0ed1c6645b4835de2
Taiye Salami <taiye41@gmail.com>
2019-05-16 10:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
+function destructivelyAppendKitten(kittens, Ralph) {
+  kittens = [...kittens, Ralph]
+  return kittens
+}
